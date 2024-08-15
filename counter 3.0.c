7646d2a6ff5eb4bd0b18615c6444dbec9ed753a8
Saulo <67021707+B0o07@users.noreply.github.com>
2024-08-15 15:21:21 -0300

    Add files via upload

---
+++ counter 3.0.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//This program will count and print meow 3 times
+int main(void)
+{
+    for (int i =0; i < 3; i++)
+    {
+        printf("Meow!\n");
+    }
+}
