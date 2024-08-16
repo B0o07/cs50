0efdbe950bb8ef1965251c0cdc31c940356caedc
Saulo <67021707+B0o07@users.noreply.github.com>
2024-08-16 16:44:30 -0300

    Add files via upload

---
+++ meow.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*void meow(void);
+
+int main(void)
+{
+    for (int i = 0; i <3; i++)
+    {
+        meow();
+    }
+}
+
+void meow(void)
+{
+    printf("meow\n");
+} */
+
+void meow(int n);
+
+int main(void)
+{
+    meow(3000);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
